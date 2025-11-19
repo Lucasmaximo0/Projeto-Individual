ad265bb264a573a3b87fe7b3ea114d082baa2b72
Lucasmaximo0 <lucas.maximo@sptech.school>
2025-11-18 23:27:25 -0300

    Realizei a melhorias nas tabelas do script do banco de dados e fiz alterações na estrutura do css do site

---
+++ Entregaveis do projeto/Banco de dados/script parcial do banco individual.sql
@@ -0,0 +1,59 @@
+CREATE DATABASE ProjetoIndividual;
+use ProjetoIndividual;
+
+
+CREATE TABLE Usuario (
+    idUsuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(45) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(45) NOT NULL,
+    dataCadastro datetime default current_timestamp
+);
+
+CREATE TABLE testemunho (
+    idtestemunho INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(500) NOT null,
+    conteudo TEXT not null,
+    dataPuplicacao datetime default current_timestamp,
+    Usuario_idUsuario INT not null,
+    FOREIGN KEY (Usuario_idUsuario) REFERENCES Usuario(idUsuario)
+);
+
+CREATE TABLE Musica (
+    idMusica INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(45) not null,
+    compositor VARCHAR(45),
+    link VARCHAR(200) not null,
+    dataCadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Usuario_idUsuario INT not null,
+    FOREIGN KEY (Usuario_idUsuario) REFERENCES Usuario(idUsuario)
+);
+
+CREATE TABLE livros (
+    idlivros INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(60) not null,
+    autor VARCHAR(45) not null,
+    ISBN VARCHAR(45),
+    dataCadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Usuario_idUsuario INT,
+    FOREIGN KEY (Usuario_idUsuario) REFERENCES Usuario(idUsuario)
+);
+
+CREATE TABLE Estudo (
+    idEstudo INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(45)not null,
+    conteudo TEXT not null,
+    dataCadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Usuario_idUsuario INT not null,
+    FOREIGN KEY (Usuario_idUsuario) REFERENCES Usuario(idUsuario)
+);
+
+CREATE TABLE comentario (
+    idcomentario INT AUTO_INCREMENT PRIMARY KEY,
+    conteudo TEXT not null,
+    dataComentario DATETIME DEFAULT CURRENT_TIMESTAMP,
+    Usuario_idUsuario INT not null,
+    testemunho_idtestemunho INT not null,
+    FOREIGN KEY (Usuario_idUsuario) REFERENCES Usuario(idUsuario),
+    FOREIGN KEY (testemunho_idtestemunho) REFERENCES testemunho(idtestemunho)
+);
